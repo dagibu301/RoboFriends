e411e0a419cbb9b04df5bfcf675fe9baf80b2e5a
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-09 13:07:56 -0500

    cards

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Card/Card";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./shared/robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      {robots.map((robot) => (
+        <Card name={robot.name} email={robot.email} username={robot.username} />
+      ))}
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
