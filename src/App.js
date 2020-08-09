d2159ec8c315a2030eb04e57504655d65cc25512
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-09 18:54:29 -0500

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -2,36 +2,47 @@ import React from "react";
 import CardList from "./CardList/CardList";
 import { robots } from "./shared/robots";
 import SearchBox from "./SearchBox/SearchBox";
-import  "./App.css";
-
+import "./App.css";
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: robots,
-            searchField: ''
-        }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }))
+      .catch(() => this.setState({ robots: robots }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filterRobots} />
+        </div>
+      );
     }
-
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-
-    }
-
-    render() {
-        const filterRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        });
-
-        return (
-            <div className="tc">
-              <h1 className="f1">RoboFriends</h1>
-              <SearchBox searchChange={this.onSearchChange} />
-              <CardList robots={filterRobots} />
-            </div>
-          );
-    }
+  }
 }
 
 export default App;
