01f933cbe1a8e4b571c38889490a93f9cb04e1e7
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-09 19:37:25 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import CardList from "./CardList/CardList";
+import CardList from "./components/CardList/CardList";
 import { robots } from "./shared/robots";
-import SearchBox from "./SearchBox/SearchBox";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
-import Scroll from "./Scroll/Scroll";
+import Scroll from "./components/Scroll/Scroll";
 
 class App extends React.Component {
   constructor() {
@@ -26,25 +26,26 @@ class App extends React.Component {
   };
 
   render() {
-    const filterRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+    const filterRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filterRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <h1 className="tl">Loading...</h1>
+      </div>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filterRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
